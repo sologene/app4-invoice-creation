e0e4aa8873f48fed4a24e82450d4d97c73c5693e
Sologene <basil.s.maliyil@gmail.com>
2024-05-03 11:34:33 +0800

    Creating a pdf output for each invoice without much representation

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
+
 filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="p",unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font(family="Times", size=16, style="B")
+    filename = Path(filepath).stem
+    invoice_name=filename.split("-")[0]
+    pdf.cell(w=50,h=8, txt=f"Invoice nr.{invoice_name}")
+    pdf.output(f"Output/{filename}.pdf")
